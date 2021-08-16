a901d612a5e2d60bf24f8407c092a57a587af2d4
Lint Action <lint-action@samuelmeuli.com>
2021-08-16 15:13:03 +0000

    Fix code style issues with Prettier

--- src/Solver.ts
+++ src/Solver.ts
@@ -260,7 +260,10 @@ export default class Solver<M, S> {
    * @param keyCounts key counts
    * @private
    */
-  private static chooseKey(domains: ModelDomains, keyCounts: {[key: string]: number}): string {
+  private static chooseKey(
+    domains: ModelDomains,
+    keyCounts: {[key: string]: number}
+  ): string {
     // apply laplace smoothing, since counts can be 0
     const laplaceAlpha = 0.1;
 
