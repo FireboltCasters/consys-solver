c4be8a82c42f63936471abd49df70a94c3c93cad
sdroppelmann <steffen.droppelmann@gmail.com>
2021-08-16 15:01:50 +0200

    added preference function export

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 /* istanbul ignore file */
 import Solver from './Solver';
-import Domain from './Domain';
+import Domain, {PreferenceFunction} from './Domain';
 import Constant from './Constant';
 import Range from './Range';
 import Set from './Set';
 
 export {Solver};
 export {Domain};
+export {PreferenceFunction};
 export {Constant};
 export {Range};
 export {Set};
